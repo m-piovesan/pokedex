34d67f80945abba8725cf2dd739fd1d4f7386951
Matheus Piovesan <matheusmoraespiovesan@gmail.com>
2024-11-08 09:46:24 -0300

    started setting layout and mixins up

---
+++ .svelte-kit/ambient.d.ts
@@ -0,0 +1,231 @@
+
+// this file is generated — do not edit it
+
+
+/// <reference types="@sveltejs/kit" />
+
+/**
+ * Environment variables [loaded by Vite](https://vitejs.dev/guide/env-and-mode.html#env-files) from `.env` files and `process.env`. Like [`$env/dynamic/private`](https://svelte.dev/docs/kit/$env-dynamic-private), this module cannot be imported into client-side code. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://svelte.dev/docs/kit/configuration#env) _and do_ start with [`config.kit.env.privatePrefix`](https://svelte.dev/docs/kit/configuration#env) (if configured).
+ * 
+ * _Unlike_ [`$env/dynamic/private`](https://svelte.dev/docs/kit/$env-dynamic-private), the values exported from this module are statically injected into your bundle at build time, enabling optimisations like dead code elimination.
+ * 
+ * ```ts
+ * import { API_KEY } from '$env/static/private';
+ * ```
+ * 
+ * Note that all environment variables referenced in your code should be declared (for example in an `.env` file), even if they don't have a value until the app is deployed:
+ * 
+ * ```
+ * MY_FEATURE_FLAG=""
+ * ```
+ * 
+ * You can override `.env` values from the command line like so:
+ * 
+ * ```bash
+ * MY_FEATURE_FLAG="enabled" npm run dev
+ * ```
+ */
+declare module '$env/static/private' {
+	export const ELECTRON_RUN_AS_NODE: string;
+	export const CHROME_DESKTOP: string;
+	export const COLORTERM: string;
+	export const DBUS_SESSION_BUS_ADDRESS: string;
+	export const DESKTOP_AUTOSTART_ID: string;
+	export const DESKTOP_SESSION: string;
+	export const DISPLAY: string;
+	export const ELECTRON_NO_ATTACH_CONSOLE: string;
+	export const GDK_BACKEND: string;
+	export const GDMSESSION: string;
+	export const GDM_LANG: string;
+	export const GNOME_DESKTOP_SESSION_ID: string;
+	export const GNOME_KEYRING_CONTROL: string;
+	export const GNOME_TERMINAL_SCREEN: string;
+	export const GNOME_TERMINAL_SERVICE: string;
+	export const GPG_AGENT_INFO: string;
+	export const GTK3_MODULES: string;
+	export const GTK_MODULES: string;
+	export const HOME: string;
+	export const LANG: string;
+	export const LANGUAGE: string;
+	export const LC_ADDRESS: string;
+	export const LC_IDENTIFICATION: string;
+	export const LC_MEASUREMENT: string;
+	export const LC_MONETARY: string;
+	export const LC_NAME: string;
+	export const LC_NUMERIC: string;
+	export const LC_PAPER: string;
+	export const LC_TELEPHONE: string;
+	export const LESSCLOSE: string;
+	export const LESSOPEN: string;
+	export const LOGNAME: string;
+	export const LS_COLORS: string;
+	export const NVM_BIN: string;
+	export const NVM_DIR: string;
+	export const NVM_INC: string;
+	export const OLDPWD: string;
+	export const ORIGINAL_XDG_CURRENT_DESKTOP: string;
+	export const PATH: string;
+	export const PWD: string;
+	export const QT_ACCESSIBILITY: string;
+	export const SESSION_MANAGER: string;
+	export const SHELL: string;
+	export const SHLVL: string;
+	export const SSH_AUTH_SOCK: string;
+	export const TERM: string;
+	export const USER: string;
+	export const VSCODE_CLI: string;
+	export const VSCODE_CODE_CACHE_PATH: string;
+	export const VSCODE_CRASH_REPORTER_PROCESS_TYPE: string;
+	export const VSCODE_CWD: string;
+	export const VSCODE_ESM_ENTRYPOINT: string;
+	export const VSCODE_HANDLES_UNCAUGHT_ERRORS: string;
+	export const VSCODE_IPC_HOOK: string;
+	export const VSCODE_NLS_CONFIG: string;
+	export const VSCODE_PID: string;
+	export const VTE_VERSION: string;
+	export const XAUTHORITY: string;
+	export const XDG_CONFIG_DIRS: string;
+	export const XDG_CURRENT_DESKTOP: string;
+	export const XDG_DATA_DIRS: string;
+	export const XDG_GREETER_DATA_DIR: string;
+	export const XDG_RUNTIME_DIR: string;
+	export const XDG_SEAT: string;
+	export const XDG_SEAT_PATH: string;
+	export const XDG_SESSION_CLASS: string;
+	export const XDG_SESSION_DESKTOP: string;
+	export const XDG_SESSION_ID: string;
+	export const XDG_SESSION_PATH: string;
+	export const XDG_SESSION_TYPE: string;
+	export const XDG_VTNR: string;
+	export const _: string;
+	export const VSCODE_L10N_BUNDLE_LOCATION: string;
+	export const ELECTRON_NO_ASAR: string;
+	export const NODE_ENV: string;
+}
+
+/**
+ * Similar to [`$env/static/private`](https://svelte.dev/docs/kit/$env-static-private), except that it only includes environment variables that begin with [`config.kit.env.publicPrefix`](https://svelte.dev/docs/kit/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Values are replaced statically at build time.
+ * 
+ * ```ts
+ * import { PUBLIC_BASE_URL } from '$env/static/public';
+ * ```
+ */
+declare module '$env/static/public' {
+	
+}
+
+/**
+ * This module provides access to runtime environment variables, as defined by the platform you're running on. For example if you're using [`adapter-node`](https://github.com/sveltejs/kit/tree/main/packages/adapter-node) (or running [`vite preview`](https://svelte.dev/docs/kit/cli)), this is equivalent to `process.env`. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://svelte.dev/docs/kit/configuration#env) _and do_ start with [`config.kit.env.privatePrefix`](https://svelte.dev/docs/kit/configuration#env) (if configured).
+ * 
+ * This module cannot be imported into client-side code.
+ * 
+ * Dynamic environment variables cannot be used during prerendering.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/private';
+ * console.log(env.DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ * 
+ * > In `dev`, `$env/dynamic` always includes environment variables from `.env`. In `prod`, this behavior will depend on your adapter.
+ */
+declare module '$env/dynamic/private' {
+	export const env: {
+		ELECTRON_RUN_AS_NODE: string;
+		CHROME_DESKTOP: string;
+		COLORTERM: string;
+		DBUS_SESSION_BUS_ADDRESS: string;
+		DESKTOP_AUTOSTART_ID: string;
+		DESKTOP_SESSION: string;
+		DISPLAY: string;
+		ELECTRON_NO_ATTACH_CONSOLE: string;
+		GDK_BACKEND: string;
+		GDMSESSION: string;
+		GDM_LANG: string;
+		GNOME_DESKTOP_SESSION_ID: string;
+		GNOME_KEYRING_CONTROL: string;
+		GNOME_TERMINAL_SCREEN: string;
+		GNOME_TERMINAL_SERVICE: string;
+		GPG_AGENT_INFO: string;
+		GTK3_MODULES: string;
+		GTK_MODULES: string;
+		HOME: string;
+		LANG: string;
+		LANGUAGE: string;
+		LC_ADDRESS: string;
+		LC_IDENTIFICATION: string;
+		LC_MEASUREMENT: string;
+		LC_MONETARY: string;
+		LC_NAME: string;
+		LC_NUMERIC: string;
+		LC_PAPER: string;
+		LC_TELEPHONE: string;
+		LESSCLOSE: string;
+		LESSOPEN: string;
+		LOGNAME: string;
+		LS_COLORS: string;
+		NVM_BIN: string;
+		NVM_DIR: string;
+		NVM_INC: string;
+		OLDPWD: string;
+		ORIGINAL_XDG_CURRENT_DESKTOP: string;
+		PATH: string;
+		PWD: string;
+		QT_ACCESSIBILITY: string;
+		SESSION_MANAGER: string;
+		SHELL: string;
+		SHLVL: string;
+		SSH_AUTH_SOCK: string;
+		TERM: string;
+		USER: string;
+		VSCODE_CLI: string;
+		VSCODE_CODE_CACHE_PATH: string;
+		VSCODE_CRASH_REPORTER_PROCESS_TYPE: string;
+		VSCODE_CWD: string;
+		VSCODE_ESM_ENTRYPOINT: string;
+		VSCODE_HANDLES_UNCAUGHT_ERRORS: string;
+		VSCODE_IPC_HOOK: string;
+		VSCODE_NLS_CONFIG: string;
+		VSCODE_PID: string;
+		VTE_VERSION: string;
+		XAUTHORITY: string;
+		XDG_CONFIG_DIRS: string;
+		XDG_CURRENT_DESKTOP: string;
+		XDG_DATA_DIRS: string;
+		XDG_GREETER_DATA_DIR: string;
+		XDG_RUNTIME_DIR: string;
+		XDG_SEAT: string;
+		XDG_SEAT_PATH: string;
+		XDG_SESSION_CLASS: string;
+		XDG_SESSION_DESKTOP: string;
+		XDG_SESSION_ID: string;
+		XDG_SESSION_PATH: string;
+		XDG_SESSION_TYPE: string;
+		XDG_VTNR: string;
+		_: string;
+		VSCODE_L10N_BUNDLE_LOCATION: string;
+		ELECTRON_NO_ASAR: string;
+		NODE_ENV: string;
+		[key: `PUBLIC_${string}`]: undefined;
+		[key: `${string}`]: string | undefined;
+	}
+}
+
+/**
+ * Similar to [`$env/dynamic/private`](https://svelte.dev/docs/kit/$env-dynamic-private), but only includes variables that begin with [`config.kit.env.publicPrefix`](https://svelte.dev/docs/kit/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Note that public dynamic environment variables must all be sent from the server to the client, causing larger network requests — when possible, use `$env/static/public` instead.
+ * 
+ * Dynamic environment variables cannot be used during prerendering.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/public';
+ * console.log(env.PUBLIC_DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ */
+declare module '$env/dynamic/public' {
+	export const env: {
+		[key: `PUBLIC_${string}`]: string | undefined;
+	}
+}
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import '../styles/global.css';
+  import './styles.scss'
+</script>
+
+<h1>hello world</h1>
+<button>
+    click me
+</button>
