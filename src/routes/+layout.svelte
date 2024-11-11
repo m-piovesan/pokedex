709634d7baae80377efd579e2c499ffa11fc6aeb
Matheus Piovesan <matheusmoraespiovesan@gmail.com>
2024-11-11 10:36:43 -0300

    styling pokedex sidebar on the right side

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,3 @@
-<!-- layout.svelte -->
 <script lang="ts">
     import '../styles/global.scss';
     import Sidebar from '../components/Sidebar/Sidebar.svelte'
@@ -9,5 +8,32 @@
 <div class="container">
     <Sidebar />
     {@render children()}
+
+    <div class="content">
+        <section>
+            <div class="screen-1"></div>
+
+            <div class="blue-buttons">
+                {#each Array(8).fill(0) as _, i}
+                    <div></div>
+                {/each}
+            </div>
+
+            <div class="white-buttons">
+                <div>
+                    {#each Array(2).fill(0) as _, i}
+                        <div></div>
+                    {/each}
+                </div>
+                
+                <div class="yellow-circle"></div> 
+            </div>
+
+            <div class="screen-2">
+                <div></div>
+                <div></div>
+            </div>
+        </section>
+    </div>
 </div>
 
