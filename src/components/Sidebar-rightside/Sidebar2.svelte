39f03ac1bcaee8a7a257777ebbbcffc588dae71d
Matheus Piovesan <matheusmoraespiovesan@gmail.com>
2024-11-12 08:48:17 -0300

    containers working fine + sidebar aswell

---
+++ src/components/Sidebar-rightside/Sidebar2.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import './styles.scss';
+</script>
+
+<div class="content">
+    <section>
+        <div class="screen-1"></div>
+
+        <div class="blue-buttons">
+            {#each Array(8).fill(0) as _, i}
+                <div></div>
+            {/each}
+        </div>
+
+        <div class="white-buttons">
+            <div>
+                {#each Array(2).fill(0) as _, i}
+                    <div></div>
+                {/each}
+            </div>
+            
+            <div class="yellow-circle"></div> 
+        </div>
+
+        <div class="screen-2">
+            <div></div>
+            <div></div>
+        </div>
+    </section>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import '../styles/global.scss';
     import Sidebar from '../components/Sidebar/Sidebar.svelte'
+    import Sidebar2 from '../components/Sidebar-rightside/Sidebar2.svelte';
     import '../styles/layout.scss';
     let { children } = $props();
 </script>
@@ -12,31 +13,6 @@
         {@render children()}
     </div>
 
-    <div class="content">
-        <section>
-            <div class="screen-1"></div>
-
-            <div class="blue-buttons">
-                {#each Array(8).fill(0) as _, i}
-                    <div></div>
-                {/each}
-            </div>
-
-            <div class="white-buttons">
-                <div>
-                    {#each Array(2).fill(0) as _, i}
-                        <div></div>
-                    {/each}
-                </div>
-                
-                <div class="yellow-circle"></div> 
-            </div>
-
-            <div class="screen-2">
-                <div></div>
-                <div></div>
-            </div>
-        </section>
-    </div>
+    <Sidebar2 />
 </div>
 
